1bd4309306f9b8290d2cd3b2577853f0df6d1714
Samuel Tauil <samueltauil@gmail.com>
2022-02-17 17:58:26 -0500

    vai santos

--- scripts/01-1-prepare-all-v1.sh
+++ scripts/01-1-prepare-all-v1.sh
@@ -1,12 +1,5 @@
 #/bin/#!/usr/bin/env bash
 
-# cleaning up leftovers
-protos=( virtualservices )
-for proto in "${protos[@]}"; do
-  for resource in $(oc get -n bookinfo "$proto" -o name); do
-    oc delete -n bookinfo "$resource";
-  done
-done
 
 # setting up all services to v1
 oc apply -f https://raw.githubusercontent.com/maistra/istio/maistra-2.1/samples/bookinfo/networking/virtual-service-all-v1.yaml
