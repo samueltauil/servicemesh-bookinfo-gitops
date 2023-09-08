d6d3f725ae72ebdafbc58d7d47e6834c9e78975d
Samuel Tauil <samueltauil@gmail.com>
2023-09-08 12:16:17 -0400

    adding kiali instance

--- scripts/load-bookinfo.sh
+++ scripts/load-bookinfo.sh
@@ -1,4 +1,5 @@
 #/bin/#!/usr/bin/env bash
 
 export GATEWAY_URL=$(oc -n istio-system get route istio-ingressgateway -o jsonpath='{.spec.host}')
+echo $GATEWAY_URL/productpage
 while :; do curl -s $GATEWAY_URL/productpage | grep -o "<title>.*</title>"; sleep 1; done
