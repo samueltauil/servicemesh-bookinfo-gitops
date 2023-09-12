002368abb05e83b64e465b5dd3a542530f5ec3d0
Samuel Tauil <samueltauil@gmail.com>
2023-09-12 11:01:45 -0400

    changing fault user test

--- scripts/04-2-prepare-delay-fault.sh
+++ scripts/04-2-prepare-delay-fault.sh
@@ -4,8 +4,8 @@
 # setting up all services to v1
 oc apply -f https://raw.githubusercontent.com/maistra/istio/maistra-2.1/samples/bookinfo/networking/virtual-service-all-v1.yaml
 
-# setting up v2 for users kadu
-curl -s https://raw.githubusercontent.com/maistra/istio/maistra-2.1/samples/bookinfo/networking/virtual-service-reviews-test-v2.yaml | sed 's/exact: jason/exact: kadu/' | oc apply -f -
+# setting up v2 for users samuel
+curl -s https://raw.githubusercontent.com/maistra/istio/maistra-2.1/samples/bookinfo/networking/virtual-service-reviews-test-v2.yaml | sed 's/exact: jason/exact: samuel/' | oc apply -f -
 
-# create a fault injection rule to delay traffic coming from the test user kadu
-curl -s https://raw.githubusercontent.com/maistra/istio/maistra-2.1/samples/bookinfo/networking/virtual-service-ratings-test-delay.yaml | sed 's/exact: jason/exact: kadu/' | oc apply -f -
+# create a fault injection rule to delay traffic coming from the test user samuel
+curl -s https://raw.githubusercontent.com/maistra/istio/maistra-2.1/samples/bookinfo/networking/virtual-service-ratings-test-delay.yaml | sed 's/exact: jason/exact: samuel/' | oc apply -f -
